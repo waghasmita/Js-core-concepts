fe9d9d3c211364b978394e80a6f2c794020abb25
waghasmita <waghasmita2126@gmail.com>
2020-07-16 11:54:41 +0530

    Updated promises js.

---
+++ promises/circle.js
@@ -0,0 +1,30 @@
+/* Animated circle with callback so that it returns a promise instead of accepting a callback. */
+
+function go() {
+  showCircle(160, 160, 100).then(div => {
+    div.classList.add('message-ball');
+    div.append("Success!");
+  });
+}
+
+function showCircle(cx, cy, radius) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'circle';
+  document.body.append(div);
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+
+      div.addEventListener('transitionend', function handler() {
+        div.removeEventListener('transitionend', handler);
+        resolve(div);
+      });
+    }, 0);
+  })
+}
